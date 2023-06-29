1fac6f16100836fd1eabf3ed3199c64dd54ca77c
Deny <deny@test.com>
2023-06-29 22:44:13 +0700

    .

--- util/0_wordpress_curl.sh
+++ util/0_wordpress_curl.sh
@@ -3,4 +3,4 @@ sed -i "s/define('FTP_USER', '\$FTP_USER');/define('FTP_USER', '$FTP_USER_NAME')
 sed -i "s/define('FTP_PASS', '\$FTP_PASS');/define('FTP_PASS', '$FTP_USER_PASSWORD');/" /var/www/html/wp-config.php
 sed -i "s/define('FTP_HOST', '\$FTP_HOST');/define('FTP_HOST', '$WEB_DOMAIN');/" /var/www/html/wp-config.php
 sed -i "s/define('DB_USER', '\$DB_USER');/define('DB_USER', '$MYSQL_USER');/" /var/www/html/wp-config.php
-sed -i "s/define('DB_NAME', '\$DB_NAME');/define('DB_PASSWORD', '$MYSQL_DATABASE');/" ./wp-config.DB_NAME
+sed -i "s/define('DB_NAME', '\$DB_NAME');/define('DB_PASSWORD', '$MYSQL_DATABASE');/" /var/www/html/wp-config.php
